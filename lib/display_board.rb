a9843df09fe070d42c16927c44091bda948e9134
John Tuttle <thejwt@gmail.com>
2017-09-19 16:57:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(displayArray)
+  puts " #{displayArray[0]} | #{displayArray[1]} | #{displayArray[2]}"
+  puts "-----------"
+  puts " #{displayArray[3]} | #{displayArray[4]} | #{displayArray[5]}"
+  puts "-----------"
+  puts " #{displayArray[6]} | #{displayArray[7]} | #{displayArray[8]}"  
+end
